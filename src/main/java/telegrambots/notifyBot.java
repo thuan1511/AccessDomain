c869d64f798b2df50431f222d9245375bcfad510
Thuan Nguyen <thuannh1511@gmail.com>
2020-07-12 20:32:24 +0700

    hehe

---
+++ src/main/java/telegrambots/notifyBot.java
@@ -0,0 +1,59 @@
+package telegrambots;
+
+
+import org.openqa.selenium.WebDriver;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class notifyBot extends TelegramLongPollingBot {
+
+	WebDriver driver;
+	private static final String CHAT_ID = "-416624316";
+//    private static final String TOKEN = "898848790:AAEJNDrQwI4tot6Y5v3Gg0VKXBytDN2pDrY";
+	
+	
+	@Override
+    public void onUpdateReceived(Update update) {
+
+    }
+
+    /**
+     * Method for creating a message and sending it.
+     * @param chatId chat id
+     * @param s The String that you want to send as a message.
+     */
+    public void sendMsg(String s) {
+        SendMessage sendMessage = new SendMessage();
+        sendMessage.enableMarkdown(true);
+        sendMessage.setChatId(CHAT_ID);
+        sendMessage.setText(s);
+        try {
+            execute(sendMessage);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+	
+	@Override
+	public String getBotUsername() {
+         return "notifyBot";
+     }
+
+	@Override
+     public String getBotToken() {
+         return "898848790:AAEJNDrQwI4tot6Y5v3Gg0VKXBytDN2pDrY";
+         // id:-332233201
+     }
+
+
+
+
+	
+
+	
+
+}
--- src/test/java/testing/API/GetRequestAutomation.java
+++ src/test/java/testing/API/GetRequestAutomation.java
@@ -1,23 +1,19 @@
 package testing.API;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.Properties;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.openqa.selenium.OutputType;
-import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.Assert;
-import org.testng.ITestResult;
-import org.testng.annotations.AfterMethod;
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
@@ -27,9 +23,8 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.Status;
 import com.aventstack.extentreports.reporter.ExtentHtmlReporter;
-import com.aventstack.extentreports.reporter.configuration.Theme;
-import com.aventstack.extentreports.utils.FileUtil;
-import com.google.common.io.Files;
+
+import telegrambots.notifyBot;
 
 public class GetRequestAutomation {
 
@@ -38,63 +33,45 @@ public class GetRequestAutomation {
 	public ExtentReports extent;
 	public static ExtentHtmlReporter htmlReporter;
 	public ExtentTest test;
-	
-	
+
+	notifyBot bot = new notifyBot();
+	public void teleBot() {
+		ApiContextInitializer.init();
+		TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+		try {
+			telegramBotsApi.registerBot(new notifyBot());
+
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+	}
+
 	@BeforeTest
 	public void setExtent() {
-		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") +"/test-output/ExtentReport.html");
-		
+		htmlReporter = new ExtentHtmlReporter(System.getProperty("user.dir") + "/test-output/ExtentReport.html");
+		htmlReporter.config().setTimeStampFormat("MMM dd, yyyy HH:mm:ss");
 		extent = new ExtentReports();
 		extent.attachReporter(htmlReporter);
-		
+
 		extent.setSystemInfo("Hostname", "localhost");
 		extent.setSystemInfo("OS", "Mac OS");
-		extent.setSystemInfo("Tester Name", "jimbi");
+		extent.setSystemInfo("Tester Name", "QC_Jimbi");
 		extent.setSystemInfo("Browser", "Chrome");
-		
+
 	}
-	
+
 	@AfterTest
 	public void endReport() {
 		extent.flush();
-		
-	}
-	@BeforeMethod
-	 public void setup() {
-	  System.setProperty("webdriver.chrome.driver", "resource/chromedriver");
-	  driver = new ChromeDriver();
-	  driver.manage().window().maximize();
-	 }
 
-	@AfterMethod
-	 public void tearDown(ITestResult result) throws IOException {
-	  if (result.getStatus() == ITestResult.FAILURE) {
-	   test.log(Status.FAIL, "TEST CASE FAILED IS " + result.getName()); // to add name in extent report
-	   test.log(Status.FAIL, "TEST CASE FAILED IS " + result.getThrowable()); // to add error/exception in extent report
-	   String screenshotPath = GetRequestAutomation.getScreenshot(driver, result.getName());
-	   test.addScreenCaptureFromPath(screenshotPath);// adding screen shot
-	  } else if (result.getStatus() == ITestResult.SKIP) {
-	   test.log(Status.SKIP, "Test Case SKIPPED IS " + result.getName());
-	  }
-	  else if (result.getStatus() == ITestResult.SUCCESS) {
-	   test.log(Status.PASS, "Test Case PASSED IS " + result.getName());
-	  }
-	  driver.quit();
-	 }
-	
-	public static String getScreenshot(WebDriver driver, String screenshotName) throws IOException {
-		  String dateName = new SimpleDateFormat("yyyyMMddhhmmss").format(new Date());
-		  TakesScreenshot ts = (TakesScreenshot) driver;
-		  File source = ts.getScreenshotAs(OutputType.FILE);
-		  
-		  // after execution, you could see a folder "FailedTestsScreenshots" under src folder
-		  String destination = System.getProperty("user.dir") + "/Screenshots/" + screenshotName + dateName + ".png";
-		  File finalDestination = new File(destination);
-//		  FileUtils.copyFile(source, finalDestination);
-		  Files.copy(source, finalDestination);
-		  return destination;
-		 }
-	
+	}
+
+	@BeforeMethod
+	public void setup() {
+		System.setProperty("webdriver.chrome.driver", "resource/chromedriver");
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
 
 	@Test
 	public void ApiTesting() throws IOException {
@@ -104,7 +81,7 @@ public class GetRequestAutomation {
 		prop.load(fis);
 
 		for (int i = 1; i <= prop.size(); i++) {
-			
+
 			String url = prop.getProperty("url" + i);
 			driver.get(url);
 
@@ -112,20 +89,20 @@ public class GetRequestAutomation {
 			HttpClient client = HttpClientBuilder.create().build();
 			HttpResponse response = client.execute(new HttpGet(url));
 			int statusCode = response.getStatusLine().getStatusCode();
-		    System.out.println(driver.getCurrentUrl() + "-----" + "status Code: " + statusCode);
+			System.out.println(driver.getCurrentUrl() + "-----" + "status Code: " + statusCode);
 			if (statusCode == 200) {
-				test.log(Status.PASS, "Access OK");
+				test.log(Status.PASS, url + "----- Access OK");
 				System.out.println("OK");
+				
 			} else {
-				test.log(Status.FAIL, "Access failed");
-				System.out.println("Failed");
+				test.log(Status.FAIL, url + "------ Access failed");
+				bot.sendMsg(driver.getCurrentUrl() + "    ------ Access Failed");
 			}
 
 		}
 		driver.quit();
 
 	}
-	
 
 }
 
